e8e4eb553df1540253f427df530d9b5466f1c59d
adrianwagemans@gmail.com <adrianwagemans@gmail.com>
2021-07-15 17:30:59 +0200

    home progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,29 +12,43 @@ const IndexPage = ({data}) => {
           
           <Slider/>
           
-            <div>
-              <h1>
-                Welcome
-              </h1>
-                <div>
+            <section className='welcomeSection'>
+                
+                  
+                      <h1>
+                      Welcome
+                    </h1>
+
+                  <div className='welcomeWraper'>
+
+                  <div className='welcomeContent'> 
+
                     <h2>
                       Welcome to Arch Studio
                     </h2>
                     <p>
                       We have a unique network and skillset to help bring your projects to life. Our small team of highly skilled individuals 
-                      combined with our large network put us in a strong position to deliver exceptional results. Over the past 10 years, 
+                      combined with our large network put us in a strong position to deliver exceptional results.
+                      </p>
+                      <p>
+                      Over the past 10 years, 
                       we have worked on all kinds of projects. From stations to high-rise buildings, we create spaces that inspire and delight. 
-                      We work closely with our clients so that we understand the intricacies of each project. 
+                      </p>
+                      <p>
+                      We work closely with our clients so that we understand the intricacies of each project.
                       This allows us to work in harmony the surrounding area to create truly stunning projects that will stand the test of time. 
                     </p>
-                </div>
-                    <StaticImage width={500} src='../images/home/desktop/image-welcome.jpg'
-                     />
+                    </div>
 
-                <div>
+                      < StaticImage className='welcomeImage' width={500} src='../images/home/desktop/image-welcome.jpg' />
+          
+                  
+              
+                  </div>
+                
+            </section>
 
-                </div>
-            </div>
+
               <div>
 
                           
